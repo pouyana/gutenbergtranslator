09afa797c46810a3f83a6a5151cd1052acc98199
Pouyan <pouyan@mail.pouyan.azari.info>
2012-12-15 18:25:50 +0100

    first working code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Gutenbergtranslator::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
   devise_for :users
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
