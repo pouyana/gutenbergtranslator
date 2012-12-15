61007aafe3b142e3f8d4736f67ae87829c4b9a80
Pouyan <pouyan@mail.pouyan.azari.info>
2012-12-15 16:34:02 +0100

    RoR is Added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gutenbergtranslator::Application.config.secret_token = '0bd53d309f2bff165da584d5c1997138e1b1a8f6357a4604dceb1643f1a65d0deb80fb03097f5c3c4bfe10a735b150756d0857e622acdbfc2a0fffb308dcffe3'
