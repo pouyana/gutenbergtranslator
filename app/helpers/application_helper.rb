f8bfbcd74dfa87dd0c336d6721f839951e120de4
Pouyan <pouyan@mail.pouyan.azari.info>
2012-12-16 01:06:02 +0100

    added the number translator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module ApplicationHelper
   def display_base_errors resource
     return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
@@ -10,4 +11,19 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
+#this is the english to persian number changer
+#it is made from php function
+  def english_to_persian_nr(num)
+   fa_num = ["۰","۱","۲","۳","۴","۵","۶","۷","۸","۹"]
+   snum = num.to_s
+   faarr = Array.new
+   snum.split("-").each do |i|
+    i.split("").each do |f|
+     fint = f.to_i
+     faarr.push(fa_num[fint])
+   end
+    faarr.push("/")
+  end
+  return faarr.join("")[0..-2]
+ end
 end
