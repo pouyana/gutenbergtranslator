085a73d6f8cb551b691bad667d6b96b342fd566e
pouyana <popeno2003@gmail.com>
2013-01-12 23:42:59 +0100

    make the lang selector work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   def self.default_url_options
     { :locale => I18n.locale }
   end
-  before_filter :set_locale
-  def set_locale
-   I18n.locale = params[:locale] || I18n.default_locale
-  end
+#  before_filter :set_locale
+#  def set_locale
+#   I18n.locale = params[:locale] || I18n.default_locale
+#  end
   helper :all
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
