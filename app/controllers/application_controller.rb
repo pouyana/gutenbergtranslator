7740a46369c359bd9b6083a3b040704231fbcc3a
pouyana <popeno2003@gmail.com>
2013-01-14 18:23:11 +0100

    working i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
+#this part is taken from ror i18l manual here:
+#http://guides.rubyonrails.org/i18n.html
+#and:
+#http://ruby-auf-schienen.de/3.2/i18n_mehrsprachige_rails_applikation.html
   def self.default_url_options
     { :locale => I18n.locale }
   end
-#  before_filter :set_locale
-#  def set_locale
-#   I18n.locale = params[:locale] || I18n.default_locale
-#  end
+  before_filter :set_locale
+  def set_locale
+   I18n.locale = params[:locale] || I18n.default_locale
+   Rails.application.routes.default_url_options[:locale]= I18n.locale 
+  end
   helper :all
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
